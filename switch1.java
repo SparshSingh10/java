103638886fa553841ffb24f688aae95bb800b75e
sparsh <sparsh262002@gmail.com>
2024-11-26 17:40:20 +0530

    261124

--- switch1.java
+++ switch1.java
@@ -1,7 +1,27 @@
 public class switch1{
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int n=7;
+
+        switch(n){ 
+            // byte short int char
+            // Byte Short Integer Character
+            // String enum
+            case 1:
+                System.out.println("Value is "+n);
+                break;
+            case 2:
+                System.out.println("Value is "+n);
+                break;
+            case 3:case 4:case 5:
+                    System.out.println("Value is "+n);
+                    break;
+            default:
+                System.out.println("Default Value is "+n);
+                break;
+
+            
+        }
     }
     
 }
