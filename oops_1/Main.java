5a17fe46256cad424eccce0da5e136c8b812e9f9
sparsh <sparsh262002@gmail.com>
2025-01-07 21:03:26 +0530

    private & getters

---
+++ oops_1/Main.java
@@ -0,0 +1,9 @@
+package oops_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        System.out.println(myCar.getMake());
+        myCar.describeCar();
+    }
+}
